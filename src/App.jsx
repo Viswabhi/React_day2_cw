e75a53c7dc05f36f9d3cdb9db6ab5bc073087b8a
Viswabhi <avishek.viswakarma@gmail.com>
2024-05-01 21:24:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+    <>
+     <div className=" mt-[150px] mx-auto w-[700px] h-[450px] text-left">
+     <h1 className='text-[38px]' >Learn web development</h1>
+      <p className="mt-6 text-[18px]">Welcome to the MDN learning area. This set of articles aims to provide complete beginners to web development with fundamental skills for coding websites.</p>
+
+      <p className="mt-6 text-[18px]">The aim is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from <a href="https://developer.mozilla.org/en-US/" className="text-blue-500 visited:text-purple-600">the rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge.</p>
+
+      <p className="mt-6 text-[18px]">If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work</p>
+     </div>
+    </>
+  )
+}
+
+export default App
